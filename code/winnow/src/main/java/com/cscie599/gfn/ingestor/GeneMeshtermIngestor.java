7510471dababb514897c5a2d81fca39f2e85455a
Lila Mullany <lilamullany@gmail.com>
2020-03-19 14:47:18 -0700

    working on gene_meshterm ingestion, currently have binding exception
    Former-commit-id: c5b37bfaaadba568a5d7baabaac581f86dec5fec

---
+++ code/winnow/src/main/java/com/cscie599/gfn/ingestor/GeneMeshtermIngestor.java
@@ -0,0 +1,118 @@
+package com.cscie599.gfn.ingestor;
+
+import com.cscie599.gfn.entities.GenePublicationPK;
+import com.cscie599.gfn.ingestor.writer.UpsertableJdbcBatchItemWriter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.io.Resource;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import javax.sql.DataSource;
+
+public class GeneMeshIngestor {
+
+    protected static final Log logger = LogFactory.getLog(GenePubmedIngester.class);
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    DataSource dataSource;
+
+    @Value("file:${input.gene_meshterm.file}")
+    private Resource inputResource;
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Bean
+    @Order(11)
+    public Job getGeneMeshtermIngester() {
+        return jobBuilderFactory.get("GeneMeshtermIngester")
+                .start(stepGeneMeshtermInfo())
+                .build();
+    }
+
+    @Bean(name = "stepGeneMeshtermInfo")
+    public Step stepGeneMeshtermInfo() {
+        return stepBuilderFactory
+                .get("stepGeneMeshtermInfo")
+                .<GenePublicationPK, GenePublicationPK>chunk(1)
+                .reader(readerForGeneMeshterm())
+                .processor(processorForGeneMeshterm())
+                .writer(writerForGeneMeshterm())
+                .faultTolerant()
+                .skip(EmptyResultDataAccessException.class)
+                .noRetry(EmptyResultDataAccessException.class)
+                .noRollback(EmptyResultDataAccessException.class)
+                .skipLimit(50000)
+                .transactionAttribute(IngeterUtil.getDefaultTransactionAttribute())
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<GenePublicationPK, GenePublicationPK> processorForGeneMeshterm() {
+        return new GeneMeshtermIngester.DBLogProcessor();
+    }
+
+    @Bean
+    public FlatFileItemReader<GenePublicationPK> readerForGenePubmed() {
+        logger.info("Reading resource: " + inputResource.getFilename() + " for "+this.getClass().getName());
+        FlatFileItemReader<GenePublicationPK> itemReader = new FlatFileItemReader<GenePublicationPK>();
+        itemReader.setLineMapper(lineMapperForGenePubmed());
+        itemReader.setResource(inputResource);
+        return itemReader;
+    }
+
+    @Bean
+    public LineMapper<GenePublicationPK> lineMapperForGenePubmed() {
+        DefaultLineMapper<GenePublicationPK> lineMapper = new DefaultLineMapper<GenePublicationPK>();
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer(DelimitedLineTokenizer.DELIMITER_TAB);
+        lineTokenizer.setNames(new String[] { "geneId", "publicationId"});
+        lineTokenizer.setIncludedFields(new int[] { 1, 2 });
+        BeanWrapperFieldSetMapper<GenePublicationPK> fieldSetMapper = new BeanWrapperFieldSetMapper<GenePublicationPK>();
+        fieldSetMapper.setStrict(true);
+        fieldSetMapper.setDistanceLimit(1);
+
+        fieldSetMapper.setTargetType(GenePublicationPK.class);
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<GenePublicationPK> writerForGenePubmed() {
+        JdbcBatchItemWriter<GenePublicationPK> itemWriter = new UpsertableJdbcBatchItemWriter<>();
+        itemWriter.setDataSource(dataSource);
+        itemWriter.setSql("INSERT INTO gene_publication (gene_id, publication_id) VALUES (:geneId, :publicationId) ON CONFLICT DO NOTHING RETURNING gene_id");
+        itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<GenePublicationPK>());
+        return itemWriter;
+    }
+
+    class DBLogProcessor implements ItemProcessor<GenePublicationPK, GenePublicationPK>
+    {
+        public GenePublicationPK process(GenePublicationPK gene) throws Exception
+        {
+            if(logger.isDebugEnabled()){
+                logger.debug("Inserting GenePublicationPK : " + gene);
+            }
+            return gene;
+        }
+    }
+}
