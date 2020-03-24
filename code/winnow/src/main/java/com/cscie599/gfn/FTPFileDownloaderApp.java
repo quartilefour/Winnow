473f2baaedf50576188ed1f4b6fb159a0ac9eb51
Pulkit Bhanot <bhanotp@uber.com>
2020-03-23 21:31:15 -0700

    fix the build runtime
    Former-commit-id: 3df079504d16fa8aed974e89de6e21fd2311e73a

--- code/winnow/src/main/java/com/cscie599/gfn/FTPFileDownloaderApp.java
+++ code/winnow/src/main/java/com/cscie599/gfn/FTPFileDownloaderApp.java
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit;
  *
  * @author PulkitBhanot
  */
-@SpringBootApplication
+//@SpringBootApplication
 public class FTPFileDownloaderApp implements CommandLineRunner {
 
 
