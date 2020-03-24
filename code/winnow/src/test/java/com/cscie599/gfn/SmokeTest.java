75253372637611a87aa9c29084826f247cf7c6fc
amv096 <amv096@g.harvard.edu>
2020-03-24 22:24:29 +0100

    Added two controllers to integration tests.
    Former-commit-id: 8af87a6eaecfc274e5c609a3f4178c1b2b7628e5

--- code/winnow/src/test/java/com/cscie599/gfn/SmokeTest.java
+++ code/winnow/src/test/java/com/cscie599/gfn/SmokeTest.java
@@ -1,10 +1,9 @@
 package com.cscie599.gfn;
 import static org.assertj.core.api.Assertions.assertThat;
+
+import com.cscie599.gfn.controller.*;
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.Test;
-import com.cscie599.gfn.controller.GeneController;
-import com.cscie599.gfn.controller.UserController;
-import com.cscie599.gfn.controller.JobLauncherController;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,11 +25,19 @@ public class SmokeTest {
     @Autowired
     private JobLauncherController jobLauncherController;
 
+    @Autowired
+    private MeshtermController meshtermController;
+
+    @Autowired
+    private StatusController statusController;
+
     @Test
     public void testContexLoads() throws Exception {
         assertThat(geneController).isNotNull();
         assertThat(userController).isNotNull();
         assertThat(jobLauncherController).isNotNull();
+        assertThat(meshtermController).isNotNull();
+        assertThat(statusController).isNotNull();
     }
 }
 
