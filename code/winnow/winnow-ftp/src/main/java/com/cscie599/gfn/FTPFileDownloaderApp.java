c1ff5708ce6c3f3192d53fc5e4387f3ce5b6bed4
Pulkit Bhanot <bhanotp@uber.com>
2020-03-24 21:03:34 -0700

    complete downloading/unzipping for gene2pubmed
    Former-commit-id: 5f208ab7d9c47284599292f35a218f7bcb18d822

--- code/winnow/winnow-ftp/src/main/java/com/cscie599/gfn/FTPFileDownloaderApp.java
+++ code/winnow/winnow-ftp/src/main/java/com/cscie599/gfn/FTPFileDownloaderApp.java
@@ -29,9 +29,10 @@ public class FTPFileDownloaderApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        CountDownLatch latch = new CountDownLatch(2);
+        CountDownLatch latch = new CountDownLatch(3);
         new Thread(new FTPFileDownloadRunnable(NCBI_FTP_SERVER_NAME, "/gene/DATA", "gene_info", "/data/raw/gene_info", latch, "/data/extracted/")).start();
         new Thread(new FTPFileDownloadRunnable(NCBI_FTP_SERVER_NAME, "/gene/DATA", "gene2go", "/data/raw/gene_info", latch, "/data/extracted/")).start();
+        new Thread(new FTPFileDownloadRunnable(NCBI_FTP_SERVER_NAME, "/gene/DATA", "gene2pubmed", "/data/raw/gene_info", latch, "/data/extracted/")).start();
 
         latch.await(30, TimeUnit.HOURS);
         if (latch.getCount() == 0) {
