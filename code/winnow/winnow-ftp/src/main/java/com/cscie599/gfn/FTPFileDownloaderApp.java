730d08706839a6c8b318b64e8b096cf204a44a09
Pulkit Bhanot <bhanotp@uber.com>
2020-03-24 23:39:43 -0700

    add support for downloading of xmlmesh files
    Former-commit-id: 6f1c86a49d01d740e40aa6fd2d5636f32329f170

--- code/winnow/winnow-ftp/src/main/java/com/cscie599/gfn/FTPFileDownloaderApp.java
+++ code/winnow/winnow-ftp/src/main/java/com/cscie599/gfn/FTPFileDownloaderApp.java
@@ -24,6 +24,7 @@ public class FTPFileDownloaderApp implements CommandLineRunner {
     protected static final Log logger = LogFactory.getLog(FTPFileDownloaderApp.class);
 
     private static String NCBI_FTP_SERVER_NAME = "ftp.ncbi.nlm.nih.gov";
+    private static String NLMPUBS_FTP_SERVER_NAME = "nlmpubs.nlm.nih.gov";
 
     public static void main(String[] args) {
         SpringApplication.run(FTPFileDownloaderApp.class, args);
@@ -32,16 +33,17 @@ public class FTPFileDownloaderApp implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        CountDownLatch latch = new CountDownLatch(7);
+        CountDownLatch latch = new CountDownLatch(8);
         new Thread(new FTPFileDownloadRunnable(NCBI_FTP_SERVER_NAME, "/gene/DATA", "gene_info", "/data/raw/gene_info", latch, "/data/extracted/gene_info/")).start();
         new Thread(new FTPFileDownloadRunnable(NCBI_FTP_SERVER_NAME, "/gene/DATA", "gene2go", "/data/raw/gene2go", latch, "/data/extracted/gene2go/")).start();
         new Thread(new FTPFileDownloadRunnable(NCBI_FTP_SERVER_NAME, "/gene/DATA", "gene2pubmed", "/data/raw/gene2pubmed", latch, "/data/extracted/gene2pubmed/")).start();
         new Thread(new FTPFileDownloadRunnable(NCBI_FTP_SERVER_NAME, "/gene/DATA", "gene_group", "/data/raw/gene_group", latch, "/data/extracted/gene_group/")).start();
+        new Thread(new FTPFileDownloadRunnable(NLMPUBS_FTP_SERVER_NAME, "/online/mesh/MESH_FILES/xmlmesh", "desc2020", "/data/raw/xmlmesh", latch, "/data/extracted/xmlmesh/")).start();
         new Thread(new HTTPGeneOntologyDownloadRunnable("gene_ontology", latch, "/data/extracted/")).start();
         new Thread(new BaselinePubmedDownloadRunnable(NCBI_FTP_SERVER_NAME,"/pubmed/baseline","pubmed", "/data/raw/pubmed", latch, "/data/extracted/")).start();
         new Thread(new IncrementalPubmedDownloadRunnable(NCBI_FTP_SERVER_NAME,"/pubmed/updatefiles","pubmed", "/data/raw/pubmed", latch, "/data/extracted/")).start();
 
-        latch.await(30, TimeUnit.HOURS);
+        latch.await(60, TimeUnit.HOURS);
         if (latch.getCount() == 0) {
             logger.info("All files downloaded successfully, exiting now");
             System.exit(0);
