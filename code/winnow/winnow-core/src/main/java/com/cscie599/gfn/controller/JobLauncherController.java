03ab7e002cced3b77cedc4d024608e43feca533a
Pulkit Bhanot <bhanotp@uber.com>
2020-03-30 09:59:21 -0700

    refractor the job launched to use ingestion service
    Former-commit-id: 2b07b717b682d11f5f1b45e8f957600c149e60b4

--- code/winnow/winnow-core/src/main/java/com/cscie599/gfn/controller/JobLauncherController.java
+++ code/winnow/winnow-core/src/main/java/com/cscie599/gfn/controller/JobLauncherController.java
@@ -1,18 +1,13 @@
 package com.cscie599.gfn.controller;
 
+import com.cscie599.gfn.service.IngestionService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 /**
  * Controller to launch the ingestion of pre downloaded datasets.
@@ -25,27 +20,14 @@ public class JobLauncherController {
     protected static final Log logger = LogFactory.getLog(JobLauncherController.class);
 
     @Autowired
-    JobLauncher jobLauncher;
-
-    @Autowired
-    List<Job> jobs;
+    private IngestionService ingestionService;
 
     @RequestMapping("/jobLauncher.html")
     public ResponseEntity handle() throws Exception {
-        logger.info("Starting the processing of different jobs");
-        JobParameters jobParameters =
-                new JobParametersBuilder().addString("date", "" + System.currentTimeMillis()).toJobParameters();
-        jobs.forEach((job -> {
-            try {
-                logger.info("Starting the processing of job:- " + job.getName());
-                long startTime = System.currentTimeMillis();
-                jobLauncher.run(job, jobParameters);
-                logger.info("Finished processing of job:- " + job.getName() + " timeTakenInMs:- " + (System.currentTimeMillis() - startTime));
-            } catch (Exception e) {
-                logger.error("Unable to complete processing of all the jobs", e);
-            }
-        }));
-        logger.info("Processing of all jobs finished");
-        return new ResponseEntity<>("Job completed successfully!", HttpStatus.OK);
+        boolean ingestionStatus = ingestionService.startIngestion();
+        if(ingestionStatus)
+            return new ResponseEntity<>("Job completed successfully!", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("All Jobs didnot completed successfully!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
