877112e94b2094417393744e983d5ba86149daec
Pulkit Bhanot <bhanotp@uber.com>
2020-03-30 10:00:35 -0700

    Change the folder structure, add add support for copying to multiple location
    Former-commit-id: 83764aab224ee2d1ccadfe6adf0a35f2504ba7b3

--- code/winnow/winnow-core/src/main/java/com/cscie599/gfn/ftp/downloader/HTTPGeneOntologyDownloadRunnable.java
+++ code/winnow/winnow-core/src/main/java/com/cscie599/gfn/ftp/downloader/HTTPGeneOntologyDownloadRunnable.java
@@ -13,7 +13,6 @@ import java.nio.file.Paths;
 import java.util.concurrent.CountDownLatch;
 
 /**
- *
  * @author PulkitBhanot
  */
 public class HTTPGeneOntologyDownloadRunnable implements Runnable {
@@ -21,47 +20,65 @@ public class HTTPGeneOntologyDownloadRunnable implements Runnable {
 
     private final String folderName;
     private final String extractedFileLocation;
+    private final String rawFileLocation;
+    private final boolean extractContent;
+
     private final CountDownLatch latch;
 
-    public HTTPGeneOntologyDownloadRunnable(String folderName,  CountDownLatch latch,String extractedFileLocation) {
+    public HTTPGeneOntologyDownloadRunnable(String folderName, CountDownLatch latch, String extractedFileLocation, String rawFileLocation, Boolean extractContent) {
         this.folderName = folderName;
         this.latch = latch;
         this.extractedFileLocation = extractedFileLocation;
+        this.rawFileLocation = rawFileLocation;
+        this.extractContent = extractContent;
     }
 
     @Override
     public void run() {
 
         try {
-            String extractedFolderLocation = this.extractedFileLocation +"go";
+            String extractedFolderLocation = this.extractedFileLocation + "gene_ontology";
             Path newDirPath = Paths.get(extractedFolderLocation);
+            if (!Files.exists(newDirPath)) {
+                Files.createDirectories(newDirPath);
+            }
+
+            String rawFolderLocation = this.rawFileLocation + "gene_ontology";
+            newDirPath = Paths.get(rawFolderLocation);
             if (!Files.exists(newDirPath)) {
                 Files.createDirectories(newDirPath);
             }
 
             FileUtils.copyURLToFile(
                     new URL("http://current.geneontology.org/ontology/subsets/goslim_agr.json"),
-                    new File(extractedFolderLocation+"/goslim_agr.json"),
+                    new File(rawFolderLocation + "/goslim_agr.json"),
                     1000,
                     60000);
 
             FileUtils.copyURLToFile(
                     new URL("http://current.geneontology.org/ontology/subsets/goslim_generic.json"),
-                    new File(extractedFolderLocation+"/goslim_generic.json"),
+                    new File(rawFolderLocation + "/goslim_generic.json"),
                     1000,
                     60000);
 
             FileUtils.copyURLToFile(
                     new URL("http://current.geneontology.org/ontology/subsets/goslim_mouse.json"),
-                    new File(extractedFolderLocation+"/goslim_mouse.json"),
+                    new File(rawFolderLocation + "/goslim_mouse.json"),
                     1000,
                     60000);
 
             FileUtils.copyURLToFile(
                     new URL("http://current.geneontology.org/ontology/subsets/goslim_plant.json"),
-                    new File(extractedFolderLocation+"/goslim_plant.json"),
+                    new File(rawFolderLocation + "/goslim_plant.json"),
                     1000,
                     60000);
+
+            if (extractContent) {
+                FileUtils.copyFile(new File(rawFolderLocation + "/goslim_agr.json"), new File(extractedFolderLocation + "/goslim_agr.json"));
+                FileUtils.copyFile(new File(rawFolderLocation + "/goslim_generic.json"), new File(extractedFolderLocation + "/goslim_generic.json"));
+                FileUtils.copyFile(new File(rawFolderLocation + "/goslim_mouse.json"), new File(extractedFolderLocation + "/goslim_mouse.json"));
+                FileUtils.copyFile(new File(rawFolderLocation + "/goslim_plant.json"), new File(extractedFolderLocation + "/goslim_plant.json"));
+            }
             latch.countDown();
         } catch (IOException e) {
             logger.error("Unable to successfully complete the processing of file " + this.folderName, e);
