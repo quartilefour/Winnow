f878d8a1999f9746a6e7c4521ec74042fd0c8173
Pulkit Bhanot <bhanotp@uber.com>
2020-03-30 10:02:36 -0700

    Build the ingestion docker container
    Former-commit-id: cd33b1e883a5febbf83b4d535f8bb3ed643f8926

--- build-code-containers.sh
+++ build-code-containers.sh
@@ -3,6 +3,7 @@ cd "code/winnow"
 ./gradlew clean build
 FILE_WAR='winnow-war/build/libs/winnow-war-0.0.1-SNAPSHOT.war'
 FILE_JAR='winnow-ftp/build/libs/winnow-ftp-0.0.1-SNAPSHOT.jar'
+INJESTION_JAR='winnow-ingest/build/libs/winnow-ingest-0.0.1-SNAPSHOT.jar'
 
 if test -f "$FILE_WAR"; then
     echo "$FILE_WAR exist"
@@ -18,9 +19,20 @@ else
     exit 1
 fi
 
+if test -f "$INJESTION_JAR"; then
+    echo "$INJESTION_JAR exist"
+else
+    echo "Code Failed to build injestion jar file"
+    exit 1
+fi
+
 cd "../.."
 docker-compose build
 
 echo "Building the winnow-ftp docker image "
 
 docker build -f ./deploy/local/ftpapp/Dockerfile --label winnow-ftpapp -t gfn_ftpapp.cscie99.com code/winnow/winnow-ftp
+
+echo "Building the winnow-ingester docker image "
+
+docker build -f ./deploy/local/ingestionapp/Dockerfile --label winnow-ftpapp -t gfn_ingesterapp.cscie99.com code/winnow/winnow-ingest
