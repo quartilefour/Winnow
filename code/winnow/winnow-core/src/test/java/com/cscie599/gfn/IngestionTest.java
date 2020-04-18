194531b0ca7d01e64347740a48b275ac94edc69e
Pulkit Bhanot <bhanotp@uber.com>
2020-04-18 13:11:34 -0700

    Add ingestion integrated tests for all the zipped datasets
    Former-commit-id: 834b42265d7f2e7d2c3fa6fc1bb99afa8efded07

--- code/winnow/winnow-core/src/test/java/com/cscie599/gfn/IngestionTest.java
+++ code/winnow/winnow-core/src/test/java/com/cscie599/gfn/IngestionTest.java
@@ -1,37 +1,90 @@
 package com.cscie599.gfn;
-import static org.assertj.core.api.Assertions.assertThat;
 
-import com.cscie599.gfn.controller.*;
+import com.cscie599.gfn.entities.*;
+import com.cscie599.gfn.repository.*;
+import com.cscie599.gfn.service.IngestionService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author PulkitBhanot
+ */
 @SpringBootTest
-public class SmokeTest extends BaseTest{
+public class IngestionTest extends BaseTest {
 
     @Autowired
-    private GeneController geneController;
+    IngestionService ingestionService;
 
     @Autowired
-    private UserController userController;
+    GeneRepository geneRepository;
 
     @Autowired
-    private JobLauncherController jobLauncherController;
+    GeneMeshtermRepository geneMeshtermRepository;
 
     @Autowired
-    private MeshtermController meshtermController;
+    MeshtermCategoryRepository meshtermCategoryRepository;
 
     @Autowired
-    private StatusController statusController;
+    MeshtermRepository meshtermRepository;
+
+    @Autowired
+    MeshtermTreeRepository meshtermTreeRepository;
+
+    @Autowired
+    PublicationRepository publicationRepository;
 
     @Test
-    public void testContextLoads() throws Exception {
-        assertThat(geneController).isNotNull();
-        assertThat(userController).isNotNull();
-        assertThat(jobLauncherController).isNotNull();
-        assertThat(meshtermController).isNotNull();
-        assertThat(statusController).isNotNull();
+    public void testRawIngestion() throws Exception {
+        ingestionService.startIngestion();
+        assertEquals("Gene count", geneRepository.findAll().size(), 26);
+        Gene gene = geneRepository.findByGeneId("1246503").get(0);
+        assertTrue(gene.getSymbol().equals("leuB"));
+        assertTrue(gene.getGeneId().equals("1246503"));
+
+        assertEquals("Gene Mesh count", geneMeshtermRepository.findAll().size(), 3);
+        assertTrue(geneMeshtermRepository.findByGeneIdOrderByPValue("5692769").size() == 1);
+        GeneMeshterm geneMeshterm = geneMeshtermRepository.findByGeneIdOrderByPValue("5692769").get(0);
+        assertTrue(geneMeshterm.getGeneMeshtermPK().getGeneId().equals("5692769"));
+        assertTrue(geneMeshterm.getGeneMeshtermPK().getMeshId().equals("D000818"));
+        assertTrue(geneMeshterm.getPublicationCount() == 12);
+        assertTrue(geneMeshterm.getPValue().equals(0.012D));
+
+        assertEquals("Meshterm category count", meshtermCategoryRepository.findAll().size(), 15);
+
+        List<MeshtermCategory> allcategories = meshtermCategoryRepository.findByCategoryId("C");
+        assertEquals(1,allcategories.size());
+        assertEquals(allcategories.get(0).getName(),"Diseases");
+        assertEquals(allcategories.get(0).getCategoryId(),"C");
+
+        assertEquals("Meshterm count", meshtermRepository.findAll().size(), 70);
+
+        List<Meshterm> meshtermList = meshtermRepository.findByMeshId("D005561");
+        assertEquals(1,meshtermList.size());
+        assertEquals("D005561",meshtermList.get(0).getMeshId());
+        assertEquals("Formates",meshtermList.get(0).getName());
+        assertEquals(" Date create for meshTerm",new Date(915177600000L),meshtermList.get(0).getDateCreated());
+        assertEquals(" Date revised for meshTerm", new Date(1341298800000L),meshtermList.get(0).getDateRevised());
+
+        assertEquals("Meshterm tree count", meshtermTreeRepository.findAll().size(), 99);
+        MeshtermTree meshtermTree = meshtermTreeRepository.findByTreeNodeId("260").get(0);
+        assertEquals("D003201",meshtermTree.getMeshtermTreePK().getMeshId());
+        assertEquals("L01.224.230",meshtermTree.getMeshtermTreePK().getTreeParentId());
+        assertEquals("260",meshtermTree.getMeshtermTreePK().getTreeNodeId());
+        assertEquals("Computers",meshtermTree.getMeshterm().getName());
+        assertEquals("D003201",meshtermTree.getMeshterm().getMeshId());
+        assertEquals(new Date(915177600000L),meshtermTree.getMeshterm().getDateCreated());
+        assertEquals(new Date(1520838000000L),meshtermTree.getMeshterm().getDateRevised());
+
+        assertEquals("Publication count", publicationRepository.findAll().size(), 9);
     }
+
 }
 
 
