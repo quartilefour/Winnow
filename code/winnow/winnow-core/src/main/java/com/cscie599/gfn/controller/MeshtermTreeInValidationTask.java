26383177ad7970f1912d4002e1d2e2372e3b1042
Pulkit Bhanot <bhanotp@uber.com>
2020-05-02 13:52:07 -0700

    Add a cache invalidation task to clear the inmemory cache of meshterm tree
    Former-commit-id: 82daac66f3991f8c69c7a84ae5f91b4db902b807

--- code/winnow/winnow-core/src/main/java/com/cscie599/gfn/controller/MeshtermTreeInValidationTask.java
+++ code/winnow/winnow-core/src/main/java/com/cscie599/gfn/controller/MeshtermTreeInValidationTask.java
@@ -1,4 +1,27 @@
 package com.cscie599.gfn.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Component to invalidate the inmemory MeshTerm tree cache every X milliseconds.
+ *
+ * @author PulkitBhanot
+ */
+@Component
 public class MeshtermTreeInValidationTask {
+
+    private static final Log logger = LogFactory.getLog(MeshtermTreeInValidationTask.class);
+
+    @Autowired
+    MeshtermController meshtermController;
+
+    @Scheduled(fixedRate = 86400000)
+    public void invalidateMeshtermCache() {
+        logger.info("Invalidating inmemory cached Meshterm tree");
+        meshtermController.invalidate();
+    }
 }
