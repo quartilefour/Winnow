d615df6b5e4bb284121bf9424439a37a4021b85e
Pulkit Bhanot <bhanotp@uber.com>
2020-05-02 13:53:17 -0700

    Make the winnow application run scheduled tasks
    Former-commit-id: 7b2df23e41eab4916f1e6cab9b0af948f8598a13

--- code/winnow/winnow-core/src/main/java/com/cscie599/gfn/WinnowApplication.java
+++ code/winnow/winnow-core/src/main/java/com/cscie599/gfn/WinnowApplication.java
@@ -2,8 +2,10 @@ package com.cscie599.gfn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WinnowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WinnowApplication.class, args);
